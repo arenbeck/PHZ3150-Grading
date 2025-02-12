bcfc8357d0997129fe604c297083aa91ed3c5704
arenbeck <ac.beck@ucf.edu>
2025-02-12 14:13:25 -0500

    rearranged functions for easier reading

--- canvas_api.py
+++ canvas_api.py
@@ -20,13 +20,11 @@ def get_submission_from_user(submissions, user):
             return submission
     return None
 
-
 def get_user_from_id(id):
     for u in users:
         if u.id == id:
             return u
     return None
-
 
 def get_user_from_name(name):
     for u in users:
@@ -121,84 +119,11 @@ def get_files_from_assignment(assignment_id, submissions_dir):
             # Extract ZIP files if detected
             extract_zip_file(dst, output_dir)
 
-
-# Old function to get the zip codes, might need to be fixed
-def old_get_files_from_assignment(assignment_id, submissions_dir):
-    if os.path.exists(submissions_dir):
-        shutil.rmtree(submissions_dir)
-    else:
-        os.makedirs(submissions_dir)
-
-    assignment = course.get_assignment(assignment_id)
-    submissions = assignment.get_submissions() # Get's submissions
-    for submission in submissions:
-        attachments = submission.attachments
-        for attachment in attachments:
-            if not any(a.filename.endswith('.zip') for a in attachments):
-                output_dir = submissions_dir + '/' + str(submission.user_id)
-                if not os.path.isdir(output_dir):
-                    os.makedirs(output_dir)
-                filePath = attachment.filename
-                attachment.download(filePath)
-                dst = os.path.join(output_dir, filePath)
-                shutil.move(filePath, dst)
-                continue
-            if not attachment.filename.endswith('.zip'): continue
-            zipFilePath = attachment.filename
-            attachment.download(zipFilePath)
-            output_dir = str(submission.user_id)
-            unzipContent(submissions_dir, zipFilePath, output_dir)
-            os.remove(zipFilePath)
-
-
 # Get's all the submissions from an assignment
 def getSubmissions(assignment_id):
     assignment = course.get_assignment(assignment_id)
     submissions = assignment.get_submissions() # Get's submissions
     return submissions
-
-# Unzips a file (might need to be fixed)
-def unzipContent(submissions_dir, zipFilePath, output_dir):
-    temp_dir = 'temp_extract'
-    if os.path.exists(temp_dir):
-        shutil.rmtree(temp_dir)
-    else:
-        os.makedirs(temp_dir)
-
-    with zipfile.ZipFile(zipFilePath, 'r') as zip_ref:
-        # Extract all contents to a temporary directory
-        zip_ref.extractall(temp_dir)
-
-    # Check if the extracted content is a folder
-    extracted_items = os.listdir(temp_dir)
-    new_folder = os.path.join(submissions_dir, output_dir)
-    os.makedirs(new_folder)
-    files = list_files_with_relative_path(temp_dir)
-
-    for file in files:
-        file_name = file.split('/')[-1]
-        dst = os.path.join(new_folder, file_name)
-        shutil.move(file, dst)
-
-    # Remove the temporary extract folder
-    shutil.rmtree(temp_dir)
-
-
-# Old helper function for unzip
-def list_files_with_relative_path(folder_path):
-    parent = os.getcwd()
-    file_list = []
-    for root, dirs, files in os.walk(folder_path):
-        directories = root.split('/')
-        if any(directory.startswith('.') for directory in directories): continue
-        for file in files:
-            if file.startswith('.'): continue
-            relative_path = os.path.relpath(os.path.join(root, file), parent)
-            if '__MACOSX' in relative_path: continue
-            file_list.append(relative_path)
-
-    return file_list
-
 
 # Grades all the students that did not submit
 def grade_not_submitted(submissions):
@@ -228,3 +153,74 @@ if __name__ == '__main__':
     assignments = course.get_assignments()
     for a in assignments:
         print(a)
+
+
+# # Old function to get the zip codes, might need to be fixed
+# def old_get_files_from_assignment(assignment_id, submissions_dir):
+#     if os.path.exists(submissions_dir):
+#         shutil.rmtree(submissions_dir)
+#     else:
+#         os.makedirs(submissions_dir)
+#
+#     assignment = course.get_assignment(assignment_id)
+#     submissions = assignment.get_submissions() # Get's submissions
+#     for submission in submissions:
+#         attachments = submission.attachments
+#         for attachment in attachments:
+#             if not any(a.filename.endswith('.zip') for a in attachments):
+#                 output_dir = submissions_dir + '/' + str(submission.user_id)
+#                 if not os.path.isdir(output_dir):
+#                     os.makedirs(output_dir)
+#                 filePath = attachment.filename
+#                 attachment.download(filePath)
+#                 dst = os.path.join(output_dir, filePath)
+#                 shutil.move(filePath, dst)
+#                 continue
+#             if not attachment.filename.endswith('.zip'): continue
+#             zipFilePath = attachment.filename
+#             attachment.download(zipFilePath)
+#             output_dir = str(submission.user_id)
+#             unzipContent(submissions_dir, zipFilePath, output_dir)
+#             os.remove(zipFilePath)
+#
+# # Unzips a file (might need to be fixed)
+# def unzipContent(submissions_dir, zipFilePath, output_dir):
+#     temp_dir = 'temp_extract'
+#     if os.path.exists(temp_dir):
+#         shutil.rmtree(temp_dir)
+#     else:
+#         os.makedirs(temp_dir)
+#
+#     with zipfile.ZipFile(zipFilePath, 'r') as zip_ref:
+#         # Extract all contents to a temporary directory
+#         zip_ref.extractall(temp_dir)
+#
+#     # Check if the extracted content is a folder
+#     extracted_items = os.listdir(temp_dir)
+#     new_folder = os.path.join(submissions_dir, output_dir)
+#     os.makedirs(new_folder)
+#     files = list_files_with_relative_path(temp_dir)
+#
+#     for file in files:
+#         file_name = file.split('/')[-1]
+#         dst = os.path.join(new_folder, file_name)
+#         shutil.move(file, dst)
+#
+#     # Remove the temporary extract folder
+#     shutil.rmtree(temp_dir)
+#
+#
+# # Old helper function for unzip
+# def list_files_with_relative_path(folder_path):
+#     parent = os.getcwd()
+#     file_list = []
+#     for root, dirs, files in os.walk(folder_path):
+#         directories = root.split('/')
+#         if any(directory.startswith('.') for directory in directories): continue
+#         for file in files:
+#             if file.startswith('.'): continue
+#             relative_path = os.path.relpath(os.path.join(root, file), parent)
+#             if '__MACOSX' in relative_path: continue
+#             file_list.append(relative_path)
+#
+#     return file_list
